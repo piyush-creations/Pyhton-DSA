26243a1cfc5c10ead83326e8147964c567afa4db
Piyush <puyeshj110@gmail.com>
2026-06-03 14:46:53 +0530

    basics

---
+++ Basics/hello.py
@@ -0,0 +1,24 @@
+print("hello, piyush!")
+# variables
+name = "Piyush"
+age = 22
+weight = 81
+#Datatypes
+a = 10 #int
+b = 10.5 #float
+c = "Piyush" #string
+name = "Piyush"     #string
+is_gym_done = True #boolean
+# printing variables
+print(name)
+print(age)
+print(weight)
+print(a)
+print(b)
+print(c)
+print(is_gym_done)
+
+# taking input from user
+name = input("Enter your name: ")
+
+print(name)
